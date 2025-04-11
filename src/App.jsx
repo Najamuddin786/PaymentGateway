ee05717853e35d4dded2a73daaf3c68d02e4b867
Md Najamuddin <786mdnajamuddin@gmail.com>
2025-04-11 15:20:21 +0530

    SETOP DONE

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PaymentPage from './Pages/paymentPage.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <>
+       <PaymentPage/>
+      </>
+  )
+}
+
+export default App
---
+++ src/Pages/PaymentPage.jsx
@@ -0,0 +1,8 @@
+import { Box,Flex,Text, } from "@chakra-ui/react";
+
+export default function PaymentPage(){
+
+    return <>
+            <h1>Payment Gateway</h1>
+    </>
+}
